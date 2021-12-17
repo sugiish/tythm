1bc51ac5076440bd07219df8b97028f2abaab379
sugiish <sugiish.me@gmail.com>
2021-12-18 03:06:43 +0900

    Add support for ping command

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import { Client } from 'discord.js'
 
-const client = new Client({ intents: 0 })
+const intents = 32767 // intents for development
+const client = new Client({ intents: intents })
 
 client.once('ready', () => {
-  console.log('準備完了！')
+  console.log('Tythm is ready!')
 })
-client.on('message', message => {
-  console.log(message.content)
+client.on('messageCreate', message => {
+  if (message.content === '!ping') {
+    // メッセージが送信されたチャンネルへ「Pong.」を送り返す。
+    message.channel.send('Pong.')
+  }
 })
 
 client.login(process.env.DISCORD_BOT_TOKEN)
