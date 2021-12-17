2433ab47946a3162622524b41bf530bd0a195119
sugiish <sugiish.me@gmail.com>
2021-12-18 02:19:25 +0900

    Use lint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import { Client } from 'discord.js';
+import { Client } from 'discord.js'
 
-const client = new Client({ intents: 0 });
+const client = new Client({ intents: 0 })
 
 client.once('ready', () => {
-  console.log('準備完了！');
-});
+  console.log('準備完了！')
+})
+client.on('message', message => {
+  console.log(message.content)
+})
 
-client.login('トークンをここに貼り付ける');
+client.login(process.env.DISCORD_BOT_TOKEN)
