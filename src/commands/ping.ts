e5aac9d1f06a209f8d27b4e77ea2bd18280fc89b
sugiish <sugiish.me@gmail.com>
2021-12-19 15:28:03 +0900

    Use discord.ts

---
+++ src/commands/ping.ts
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+import { User, CommandInteraction } from 'discord.js'
+import { Discord, Slash, SimpleCommand, SimpleCommandMessage } from 'discordx'
+
+@Discord()
+abstract class PingCommand {
+  private ping (user: User): string {
+    return `${user} Pong!`
+  }
+
+  @Slash('ping')
+  private pingSlash (interaction: CommandInteraction) {
+    const reply = this.ping(interaction.user)
+    interaction.reply(reply)
+  }
+
+  @SimpleCommand('ping')
+  private pingSimpleCommand (command: SimpleCommandMessage) {
+    const reply = this.ping(command.message.author)
+    command.message.reply(reply)
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,32 @@
-import { Client } from 'discord.js'
+import { Client } from 'discordx'
+import { Interaction, Intents } from 'discord.js'
+import { importx } from '@discordx/importer'
 import { loadConfig } from './config'
+import path from 'path'
 
-const intents = 32767 // intents for development
-const client = new Client({ intents: intents })
+const intents = [
+  Intents.FLAGS.GUILDS,
+  Intents.FLAGS.GUILD_MESSAGES,
+  Intents.FLAGS.GUILD_VOICE_STATES
+]
 const config = loadConfig()
+const client = new Client({ intents: intents })
 
-client.once('ready', () => {
+client.once('ready', async () => {
+  await client.initApplicationCommands()
+  await client.initApplicationPermissions()
   console.log('Tythm is ready!')
 })
-client.on('messageCreate', message => {
-  if (message.content === '!ping') {
-    // メッセージが送信されたチャンネルへ「Pong.」を送り返す。
-    message.channel.send('Pong.')
-  }
+client.on('interactionCreate', (interaction: Interaction) => {
+  client.executeInteraction(interaction)
+})
+client.on('messageCreate', (message) => {
+  client.executeCommand(message)
 })
 
-client.login(config.discordBotToken)
+async function start (client: Client) {
+  await importx(path.join(__dirname, 'commands/**/*.{js,ts}'))
+  await client.login(config.discordBotToken)
+}
+
+start(client)
